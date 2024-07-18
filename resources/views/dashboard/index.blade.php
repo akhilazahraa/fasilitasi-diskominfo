8329a89f093528cbcc1824b477b4f1efab9fdbfb
akhilazahraa <akhilazahra03@gmail.com>
2024-07-18 07:52:53 +0700

    add: calendar

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('container')
+    <div>
+        <div id='calendar'></div>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
