9cba93b47410304bfec706c68a205009f5ab78f4
brilliahib <brilliahib21@gmail.com>
2024-08-08 11:25:01 +0700

    fix: change layout pdf

--- resources/views/dashboard/events/details/pdf.blade.php
+++ resources/views/dashboard/events/details/pdf.blade.php
@@ -1,32 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Detail Acara</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         * {
             font-family: Arial, sans-serif;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            font-size: 14px;
-        }
-
-        table,
-        th,
-        td {
-            border: 1px solid black;
-        }
-
-        th,
-        td {
-            padding: 8px;
-            text-align: left;
         }
 
         .kop-surat {
@@ -56,75 +40,149 @@
         .date {
             font-size: 14px;
         }
+
+        .text-muted {
+            color: rgba(33, 37, 41, 0.75)
+        }
+
+        .d-flex {
+            display: flex !important;
+        }
     </style>
 </head>
 
 <body>
-    <div class="kop-surat d-flex">
+    <div class="kop-surat">
+        {{-- <div class="col">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Coat_of_arms_of_Central_Java.svg/789px-Coat_of_arms_of_Central_Java.svg.png"
+                alt="">
+        </div> --}}
         <div class="text-center">
-            <h4 class="fw-bold">PEMERINTAH PROVINSI JAWA TENGAH</h4>
-            <h2 class="fw-bold">DINAS KOMUNIKASI DAN INFORMATIKA</h2>
+            <h4 class="fw-bold mb-1">PEMERINTAH PROVINSI JAWA TENGAH</h4>
+            <h2 class="fw-bold mb-2">DINAS KOMUNIKASI DAN INFORMATIKA</h2>
             <div class="alamat">
                 <p class="mb-0">Jalan Menteri Supeno I Nomor 2 Semarang 50234</p>
                 <p class="mb-0">Telepone 024-8319140 Faksimile (024)8319328</p>
-                <p class="mb-0">Laman: <a
-                        href="http://diskominfo.jatengprov.go.id">http://diskominfo.jatengprov.go.id</a></p>
-                <p class="mb-0">Surat Elektronik: <a
-                        href="mailto:diskominfo@jatengprov.go.id">diskominfo@jatengprov.go.id</a></p>
+                <p class="mb-0">
+                    Laman:
+                    <a href="http://diskominfo.jatengprov.go.id">http://diskominfo.jatengprov.go.id</a>
+                    Surat Elektronik:
+                    <a href="mailto:diskominfo@jatengprov.go.id">diskominfo@jatengprov.go.id</a>
+                </p>
             </div>
         </div>
     </div>
     <div class="divider"></div>
     <div class="mb-4">
-        <p class="text-end date">{{ \Carbon\Carbon::now()->locale('id')->translatedFormat('l, d F Y') }}</p>
-        <h6 class="fw-bold">Kegiatan Fasilitasi {{ $event->instansi->name }}</h6>
+        <div class="text-center">
+            <h4 class="fw-bold">LAPORAN PELAKSANAAN FASILITASI</h4>
+            <h4 class="fw-bold text-uppercase">{{ $event->instansi->name }}</h4>
+        </div>
     </div>
-    <table class="table table-striped">
+    <table class="table table-borderless">
         <thead>
             <tr>
-                <th>No</th>
-                <th>Nama Acara</th>
-                <th>OPD</th>
-                <th>ISP</th>
-                <th>Tim</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
             </tr>
         </thead>
-        <tbody>
+        <tbody style="line-height: 2.5">
             <tr>
-                <td>1</td>
-                <td>{{ $event->name }}</td>
-                <td>{{ $event->instansi->name }}</td>
-                <td>{{ $event->providers->name }}</td>
-                <td>
-                    <ol class="mb-0">
+                <td>Nama Kegiatan</td>
+                <td>: {{ $event->name }}</td>
+            </tr>
+            <tr>
+                <td>OPD</td>
+                <td>: {{ $event->instansi->name }}</td>
+            </tr>
+            <tr>
+                <td>ISP</td>
+                <td>: {{ $event->providers->name }}</td>
+            </tr>
+            <tr>
+                <td>Waktu Pelaksanaan</td>
+                <td>: {{ \Carbon\Carbon::parse($event->start)->locale('id')->translatedFormat('l, d F Y, H:i') }}</td>
+            </tr>
+            <tr>
+                <td>Lokasi</td>
+                <td>: {{ $event->location }}<</td>
+            </tr>
+            <tr>
+                <td>Jam</td>
+                <td>: {{ \Carbon\Carbon::parse($event->start)->locale('id')->translatedFormat('H:i') }}
+                    - Selesai<</td>
+            </tr>
+            <tr>
+                <td>Tim Pelaksana</td>
+                <td><ol>
+                    @foreach ($event->tims as $tim)
+                        <li>{{ $tim->name }}</li>
+                    @endforeach
+                </ol></td>
+            </tr>
+        </tbody>
+    </table>
+    {{-- <section>
+        <ol>
+            <li>
+                <div class="d-flex justify-content-between">
+                    <p>Nama Kegiatan :</p>
+                    <p>{{ $event->name }}</p>
+                </div>
+            </li>
+            <li>
+                <div class="d-flex">
+                    <p>OPD :</p>
+                    <p>{{ $event->instansi->name }}</p>
+                </div>
+            </li>
+            <li>
+                <div class="d-flex">
+                    <p>ISP :</p>
+                    <p>{{ $event->providers->name }}</p>
+                </div>
+            </li>
+            <li>
+                <div class="d-flex">
+                    <p>Pelaksanaan Kegiatan :</p>
+                    <ol type="a">
+                        <li>
+                            <div>
+                                <p>Waktu Pelaksanaan :</p>
+                                <p>{{ \Carbon\Carbon::parse($event->start)->locale('id')->translatedFormat('l, d F Y, H:i') }}
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <p>Lokasi :</p>
+                                <p>{{ $event->location }}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <p>Jam :</p>
+                                <p>{{ \Carbon\Carbon::parse($event->start)->locale('id')->translatedFormat('H:i') }}
+                                    - Selesai</p>
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+            </li>
+            <li>
+                <div class="d-flex">
+                    <p>Tim Pelaksana :</p>
+                    <ol type="a">
                         @foreach ($event->tims as $tim)
                             <li>{{ $tim->name }}</li>
                         @endforeach
                     </ol>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Tanggal Mulai</th>
-                <th>Tanggal Selesai</th>
-                <th>Kebutuhan</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>{{ \Carbon\Carbon::parse($event->start)->locale('id')->translatedFormat('l, d F Y, H:i') }}</td>
-                <td>{{ \Carbon\Carbon::parse($event->end)->locale('id')->translatedFormat('l, d F Y, H:i') }}</td>
-                <td>{{ $event->kebutuhan }}</td>
-                <td>{{ $event->status }}</td>
-            </tr>
-        </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeo7lAiBX1TH4OVFFzFn24UwUlgp3jl8a8dKA82UdwBI8BJ3" crossorigin="anonymous">
+                </div>
+            </li>
+        </ol>
+    </section> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 </body>
 
